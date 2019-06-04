e301263322cdba53b7f7704237378d008c3f14d7
James Fulford <james.fulford@outlook.com>
2019-06-03 22:59:04 -0400

    refactor(app): Todo route refactor

--- routes/todos.js
+++ routes/todos.js
@@ -12,37 +12,36 @@ function respondWithError(res) {
     }
 }
 
-router.get("/", (req, res) => {
-    Todo.find()
-        .then(res.json.bind(res))
-        .catch(respondWithError(res));
-});
+router.route('/')
+    .get((req, res) => {
+        Todo.find()
+            .then(res.json.bind(res))
+            .catch(respondWithError(res))
+    })
+    .post((req, res) => {
+        Todo.create(req.body)
+            .then(res.status(201).json.bind(res))
+            .catch(respondWithError(res));
+    });
 
-router.post('/', (req, res) => {
-    Todo.create(req.body)
-        .then(res.status(201).json.bind(res))
-        .catch(respondWithError(res));
-});
-
-router.get('/:id', (req, res) => {
-    Todo.findById(req.params.id)
-        .then(res.json.bind(res))
-        .catch(respondWithError(res));
-});
-
-router.put('/:_id', (req, res) => {
-    Todo.findOneAndUpdate(req.params, req.body, { new: true })
-        .then(res.json.bind(res))
-        .catch(respondWithError(res));
-});
-
-router.delete('/:_id', (req, res) => {
-    Todo.remove(req.params)
-        .then(() => res.json({
-            message: `Successfully deleted ${req.params._id}`
-        }))
-        .catch(respondWithError(res));
-});
+router.route('/:_id')
+    .get((req, res) => {
+        Todo.findById(req.params._id)
+            .then(res.json.bind(res))
+            .catch(respondWithError(res));
+    })
+    .put((req, res) => {
+        Todo.findOneAndUpdate(req.params, req.body, { new: true })
+            .then(res.json.bind(res))
+            .catch(respondWithError(res));
+    })
+    .delete((req, res) => {
+        Todo.remove(req.params)
+            .then(() => res.json({
+                message: `Successfully deleted ${req.params._id}`
+            }))
+            .catch(respondWithError(res));
+    });
 
 module.exports = {
     default: router,
