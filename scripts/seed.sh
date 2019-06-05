9d2162961aaf557e2aeea96fca416a75d806bbe1
James Fulford <james.fulford@outlook.com>
2019-06-04 20:17:36 -0400

    WIP(scripts): Add seeded completed todo

--- scripts/seed.sh
+++ scripts/seed.sh
@@ -3,3 +3,9 @@ curl -X POST http://127.0.0.1:4242/api/todos \
 -d '{
     "name": "Create a todo"
 }'
+curl -X POST http://127.0.0.1:4242/api/todos \
+--header "Content-Type: application/json" \
+-d '{
+    "name": "Create a completed todo",
+    "completed": true
+}'
