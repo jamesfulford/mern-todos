fb9a19f10139fcc17034470f2baf9a44ad730dfd
James Fulford <james.fulford@outlook.com>
2019-06-04 20:14:17 -0400

    feat(view): Render index route results

--- public/app.js
+++ public/app.js
@@ -1,7 +1,14 @@
 $(document).ready(function () {
     $.getJSON('/api/todos')
         .done(function(todos) {
-            console.info(todos);
+            var todoElements = todos.map(function(todo) {
+                return $('<li>' + todo.name + '</li>')
+                    .addClass('task');
+            });
+            var list = $('.list');
+            todoElements.forEach(function(todoElement) {
+                list.append(todoElement);
+            });
         })
         .fail(console.error);
 });
