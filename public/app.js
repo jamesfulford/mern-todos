ba51a76ce6c6082dcd819bd15830c9fbdbf08f65
James Fulford <james.fulford@outlook.com>
2019-06-04 20:07:57 -0400

    WIP(view): Fetching todos

--- public/app.js
+++ public/app.js
@@ -1,1 +1,7 @@
-alert('Hello World!');
+$(document).ready(function () {
+    $.getJSON('/api/todos')
+        .done(function(todos) {
+            console.info(todos);
+        })
+        .fail(console.error);
+});
